e1b65601f8c8ec9d9e553f67cc3cc7d89e50e7a7
S1ro1 <matej.sirovatka@gmail.com>
2024-01-09 00:45:14 +0100

    Abstract blog title to a component

--- app/blog/new/page.tsx
+++ app/blog/new/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import {Input} from "@/components/ui/input";
 import {ResizableHandle, ResizablePanel, ResizablePanelGroup} from "@/components/ui/resizable";
 import Markdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
@@ -16,10 +15,10 @@ import {showToast} from "@/lib/utils";
 
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { dracula } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import {BlogTitle} from "@/components/blog-title";
 
 export default function Page() {
   const [text, setText] = useState<string>("");
-  const [title, setTitle] = useState<string>("");
   const router = useRouter();
 
   const handleKeyDown = (e: any) => {
@@ -64,18 +63,7 @@ export default function Page() {
   return (
     <div className={"flex justify-center h-[90vh]"}>
       <form className={"flex flex-col w-[80%] items-start gap-y-4 h-[90%]"} action={insertPost}>
-        <div className={"flex flex-row text-left w-full items-center"}>
-          <Input
-            type={"text"}
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            onKeyDownCapture={handleKeyDown}
-            placeholder={"Title..."}
-            className={"w-1/2 text-2xl min-h-[52px]"}
-            name={"title"}
-          />
-          <h1 className={"text-4xl mx-5"}>{title}</h1>
-        </div>
+        <BlogTitle />
         <ResizablePanelGroup direction="horizontal" className={"relative"}>
           <ResizablePanel defaultSize={50}>
             <Textarea
